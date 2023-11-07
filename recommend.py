e7588631309e51ef44c73c8a08a57b11b614d894
Shreyash Mohod <64720401+shree-cloud@users.noreply.github.com>
2023-11-07 20:24:11 +0530

    Update recommend.py

--- recommend.py
+++ recommend.py
@@ -64,4 +64,4 @@ def recommend():
     return render_template('index.html', recommendations=recommendations)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
